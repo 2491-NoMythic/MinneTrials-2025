0054c0cd8ba4da5a454015ac347b86e02da69210
Programming7 <2491NoMythic@gmail.com>
2025-10-28 20:31:32 -0500

    Drivetrain base code created

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
+
+
+/** An example command that uses an example subsystem. */
+public class Drive extends Command {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private DriveTrain commandDriveTrain;
+  private Joystick commandJoystick;
+
+  private Drive drive;
+
+  public Drive(DriveTrain drivesystem, Joystick drivejoy) {
+    commandDriveTrain = drivesystem;
+    commandJoystick = drivejoy;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivesystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // Uses the getThrottle function thing to set how much we have moved our throttle(the little switch)
+    double yAxis = commandJoystick.getY();
+
+    // If the absolute value of our throttle(how much we are moving the stick forward or back) is less than our deadzone, don't move.
+    if (Math.abs(yAxis) < Constants.deadZone){
+      yAxis = 0;
+    }
+    // Uses our previously made Arcade drive and throttle to set how much moving the joystick will rotate or move the robot)
+      commandDriveTrain.adrive(yAxis*Constants.maxDrivetrainPow, commandJoystick.getZ()*-0.50);
+      
+    // double curPosition = commandDrivetrain.GetPosition();
+    // System.out.println(curPosition);
+     
+      double curSpeed = commandDriveTrain.GetSpeed();
+      System.out.println(curSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,69 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.OperatorConstants;
+
+public class DriveTrain extends SubsystemBase {
+  private final TalonFX LeftDrive; 
+  private final TalonFX RightDrive;
+  private final DifferentialDrive BothDrive;
+  
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+    LeftDrive = new TalonFX((OperatorConstants.leftDriveID));
+    RightDrive = new TalonFX((OperatorConstants.rightDriveID));
+    BothDrive = new DifferentialDrive(LeftDrive::set, RightDrive::set);
+
+    LeftDrive.setNeutralMode(NeutralModeValue.Brake);
+    LeftDrive.setSafetyEnabled(false);
+    RightDrive.setNeutralMode(NeutralModeValue.Brake);
+    RightDrive.setSafetyEnabled(false);
+
+    
+
+
+  }
+
+  public void adrive(double speed, double rotation){
+    BothDrive.arcadeDrive(speed, rotation);
+  }
+
+  public void tdrive(double lspeed, double rspeed){
+    BothDrive.tankDrive(lspeed, rspeed);
+  }
+
+  public void stop(){
+    BothDrive.tankDrive(0, 0);
+  }
+
+  public Double GetPosition(){
+    return LeftDrive.getPosition().getValueAsDouble(); 
+  }
+
+  public Double GetSpeed(){
+    return LeftDrive.getVelocity().getValueAsDouble()/12*-1; 
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
